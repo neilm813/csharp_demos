ecbe71de8ba2115ab3ad7fdde88abaf97f47c098
Neil M <neilm813@gmail.com>
2022-02-22 11:00:39 -0800

    future date validation

---
+++ TripPlanner/CustomValidators.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TripPlanner
+{
+    public class FutureDate : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime date = (DateTime)value;
+
+            if (date <= DateTime.Now)
+            {
+                return new ValidationResult("must be in the future.");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
