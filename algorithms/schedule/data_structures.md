2ab0ae24ad5280b7b09a3b5a9528490124cebebc
Neil M <neilm813@gmail.com>
2022-04-01 12:18:48 -0700

    starting template

---
+++ algorithms/schedule/data_structures.md
@@ -0,0 +1,43 @@
+# [Data Structures](../src/data_structures) Algo Schedule
+
+- [recursion review](../src/recursion/intro-notes/intro.md)
+
+---
+
+## [Week 1 - Singly Linked Lists](../src/data_structures/SinglyLinkedList/index.js)
+
+---
+
+## [Week 2 - Binary Search Tree](../src/data_structures/BinarySearchTree/index.js)
+
+---
+
+## Week 3 - Min Heap & Linked Lists Part 2
+
+### Mon - [Min Heap](../src/data_structures/MinHeap/index.js)
+
+### Tue - Min Heap
+
+### Wed - [SinglyLinkedList](../src/data_structures/SinglyLinkedList/index.js)
+
+### Thur - [DoublyLinkedList](../src/data_structures/DoublyLinkedList/index.js)
+
+### Fri - [DoublyLinkedList](../src/data_structures/DoublyLinkedList/index.js)
+
+---
+
+## Week 4 - Stacks and Queues
+
+### Mon - [Stacks](../src/data_structures/Stack/index.js)
+
+- [intro](../src/data_structures/Queue/intro.md)
+
+### Tue - Queue
+
+- [Queue.js](../src/data_structures/Queue/index.js)
+
+### Wed - Queue
+
+### Thur - Queue
+
+### Fri - PriorityQueue
---
+++ algorithms/src/data_structures/Queue/index.js
@@ -0,0 +1,60 @@
+/**
+ * Class to represent a queue using an array which follows a FIFO
+ * (First In First Out) order. New items are added to the back and items are
+ * removed from the front.
+ */
+class Queue {
+  /**
+   *
+   * @param {Array<any>} items The starting items, useful for if you already
+   *    have an array of items in the right order and want to convert it to a
+   *    queue instance to get access to the queue methods.
+   */
+  constructor(items = []) {
+    this.items = items;
+  }
+
+  /**
+   * Adds a new given item to the back of this queue.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @param {any} item The new item to add to the back.
+   * @returns {number} The new size of this queue.
+   */
+  enqueue(item) {}
+
+  /**
+   * Removes and returns the first item of this queue.
+   * - Time: O(n) linear, due to having to shift all the remaining items to
+   *    the left after removing first elem.
+   * - Space: O(1) constant.
+   * @returns {any} The first item or undefined if empty.
+   */
+  dequeue() {}
+
+  /**
+   * Retrieves the first item without removing it.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @returns {any} The first item or undefined if empty.
+   */
+  front() {}
+
+  /**
+   * Returns whether or not this queue is empty.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @returns {boolean}
+   */
+  isEmpty() {}
+
+  /**
+   * Retrieves the size of this queue.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @returns {number} The length.
+   */
+  size() {}
+}
+
+/* Rebuild the above class using a linked list instead of an array. */
