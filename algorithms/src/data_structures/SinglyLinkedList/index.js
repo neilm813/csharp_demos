117670191603f2bea009178059e03f5e1dbe7644
Neil M <neilm813@gmail.com>
2022-04-06 09:02:00 -0700

    algo reqs

--- algorithms/src/data_structures/SinglyLinkedList/index.js
+++ algorithms/src/data_structures/SinglyLinkedList/index.js
@@ -42,6 +42,34 @@ class SinglyLinkedList {
   }
 
   /**
+   * Removes the last node of this list.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @returns {any} The data from the node that was removed.
+   */
+  removeBack() {}
+
+  /**
+   * Determines whether or not the given search value exists in this list.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {any} val The data to search for in the nodes of this list.
+   * @returns {boolean}
+   */
+  contains(val) {}
+
+  /**
+   * Determines whether or not the given search value exists in this list.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {any} val The data to search for in the nodes of this list.
+   * @param {?ListNode} current The current node during the traversal of this list
+   *    or null when the end of the list has been reached.
+   * @returns {boolean}
+   */
+  containsRecursive(val, current = this.head) {}
+
+  /**
    * Creates a new node with the given data and inserts that node at the front
    * of the list.
    * - Time: O(1) constant.
