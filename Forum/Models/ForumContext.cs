cb5a09bd9ae82c2080604358fc2423d5a1322f82
Neil M <neilm813@gmail.com>
2022-04-13 10:52:45 -0700

    init proj

---
+++ Forum/Models/ForumContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.Models
+{
+    public class ForumContext : DbContext
+    {
+        public ForumContext(DbContextOptions options) : base(options) { }
+
+        // for every model / entity that is going to be part of the db
+        // the names of these properties will be the names of the tables in the db
+        DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ Forum/Models/Post.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Models
+{
+    public class Post
+    {
+        [Key] // Primary Key
+        public int PostId { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Must be more than 2 characters.")]
+        [MaxLength(45, ErrorMessage = "Must be less than 45 characters.")]
+        public string Topic { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Must be more than 2 characters.")]
+        public string Body { get; set; }
+
+        [Display(Name = "Image Url")]
+        public string ImgUrl { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
