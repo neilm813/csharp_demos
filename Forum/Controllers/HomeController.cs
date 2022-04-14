d1d713bcaaf09877de153668de1c1b5d4dfd668c
Neil M <neilm813@gmail.com>
2022-04-14 11:43:11 -0700

    login reg

--- Forum/Controllers/HomeController.cs
+++ Forum/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Forum.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
 
 namespace Forum.Controllers
 {
@@ -24,6 +26,76 @@ namespace Forum.Controllers
         public IActionResult Index()
         {
             return View("Index");
+        }
+
+        [HttpPost("/register")]
+        public IActionResult Register(User newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isEmailTaken = db.Users.Any(u => u.Email == newUser.Email);
+
+                if (isEmailTaken)
+                {
+                    ModelState.AddModelError("Email", "is taken.");
+                }
+            }
+
+            // Display any errors that were manually added above.
+            if (ModelState.IsValid == false)
+            {
+                return View("Index");
+            }
+
+            PasswordHasher<User> hasher = new PasswordHasher<User>();
+            newUser.Password = hasher.HashPassword(newUser, newUser.Password);
+
+            db.Users.Add(newUser);
+            db.SaveChanges();
+
+            // After save changes, newUser has been updated with the Primary Key
+            HttpContext.Session.SetInt32("UserId", newUser.UserId);
+            HttpContext.Session.SetString("FullName", newUser.FullName());
+            return RedirectToAction("All", "Posts");
+        }
+
+        [HttpPost("/login")]
+        public IActionResult Login(LoginUser loginUser)
+        {
+            if (ModelState.IsValid == false)
+            {
+                // Display Errors
+                return View("Index");
+            }
+
+            User dbUser = db.Users.FirstOrDefault(u => u.Email == loginUser.LoginEmail);
+
+            if (dbUser == null)
+            {
+                ModelState.AddModelError("LoginEmail", "not found.");
+                // Show errors on form.
+                return View("Index");
+            }
+
+            PasswordHasher<LoginUser> hasher = new PasswordHasher<LoginUser>();
+            PasswordVerificationResult pwCompareResult = hasher.VerifyHashedPassword(loginUser, dbUser.Password, loginUser.LoginPassword);
+
+            if (pwCompareResult == 0)
+            {
+                ModelState.AddModelError("LoginPassword", "invalid.");
+                return View("Index");
+            }
+
+            HttpContext.Session.SetInt32("UserId", dbUser.UserId);
+            HttpContext.Session.SetString("FullName", dbUser.FullName());
+            return RedirectToAction("All", "Posts");
+        }
+
+        [HttpPost("/logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
--- Forum/Models/ForumContext.cs
+++ Forum/Models/ForumContext.cs
@@ -8,6 +8,7 @@ namespace Forum.Models
 
         // for every model / entity that is going to be part of the db
         // the names of these properties will be the names of the tables in the db
+        public DbSet<User> Users { get; set; }
         public DbSet<Post> Posts { get; set; }
     }
 }
