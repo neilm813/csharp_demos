8fc9df04da7a614d5c072d61fb5f1926a7bd04e3
Neil M <neilm813@gmail.com>
2022-04-19 10:41:17 -0700

    proj setup

---
+++ BeltPrep/Controllers/HomeController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BeltPrep.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+
+namespace BeltPrep.Controllers
+{
+    public class HomeController : Controller
+    {
+        private int? uid
+        {
+            get
+            {
+                return HttpContext.Session.GetInt32("UserId");
+            }
+        }
+        private readonly ILogger<HomeController> _logger;
+        private BeltPrepContext db;
+
+        public HomeController(BeltPrepContext context, ILogger<HomeController> logger)
+        {
+            db = context;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            if (uid != null)
+            {
+                return RedirectToAction("All", "Trips");
+            }
+            return View("Index");
+        }
+
+        [HttpPost("/register")]
+        public IActionResult Register(User newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isEmailTaken = db.Users.Any(u => u.Email == newUser.Email);
+
+                if (isEmailTaken)
+                {
+                    ModelState.AddModelError("Email", "is taken.");
+                }
+            }
+
+            // Display any errors that were manually added above.
+            if (ModelState.IsValid == false)
+            {
+                return View("Index");
+            }
+
+            PasswordHasher<User> hasher = new PasswordHasher<User>();
+            newUser.Password = hasher.HashPassword(newUser, newUser.Password);
+
+            db.Users.Add(newUser);
+            db.SaveChanges();
+
+            // After save changes, newUser has been updated with the Primary Key
+            HttpContext.Session.SetInt32("UserId", newUser.UserId);
+            HttpContext.Session.SetString("FullName", newUser.FullName());
+            return RedirectToAction("All", "Trips");
+        }
+
+        [HttpPost("/login")]
+        public IActionResult Login(LoginUser loginUser)
+        {
+            if (ModelState.IsValid == false)
+            {
+                // Display Errors
+                return View("Index");
+            }
+
+            User dbUser = db.Users.FirstOrDefault(u => u.Email == loginUser.LoginEmail);
+
+            if (dbUser == null)
+            {
+                ModelState.AddModelError("LoginEmail", "not found.");
+                // Show errors on form.
+                return View("Index");
+            }
+
+            PasswordHasher<LoginUser> hasher = new PasswordHasher<LoginUser>();
+            PasswordVerificationResult pwCompareResult = hasher.VerifyHashedPassword(loginUser, dbUser.Password, loginUser.LoginPassword);
+
+            if (pwCompareResult == 0)
+            {
+                ModelState.AddModelError("LoginPassword", "invalid.");
+                return View("Index");
+            }
+
+            HttpContext.Session.SetInt32("UserId", dbUser.UserId);
+            HttpContext.Session.SetString("FullName", dbUser.FullName());
+            return RedirectToAction("All", "Trips");
+        }
+
+        [HttpPost("/logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BeltPrep/Controllers/TripsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BeltPrep.Models;
+
+namespace BeltPrep.Controllers
+{
+    public class TripsController : Controller
+    {
+        private BeltPrepContext db;
+
+        public TripsController(BeltPrepContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet("/trips")]
+        public IActionResult All()
+        {
+            return View("All");
+        }
+    }
+}
