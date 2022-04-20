d3c0d07bfe2c47e5dd2abbd7920ac29badd8575f
Neil M <neilm813@gmail.com>
2022-04-20 09:04:33 -0700

    algos

--- algorithms/src/data_structures/SinglyLinkedList/index.js
+++ algorithms/src/data_structures/SinglyLinkedList/index.js
@@ -66,6 +66,32 @@ class SinglyLinkedList {
   }
 
   /**
+   * Reverses this list in-place without using any extra lists.
+   * - Time: (?).
+   * - Space: (?).
+   * @returns {SinglyLinkedList} This list.
+   */
+  reverse() {}
+
+  /**
+   * Determines whether the list has a loop in it which would result in
+   * infinitely traversing unless otherwise avoided. A loop is when a node's
+   * next points to a node that is behind it.
+   * - Time: (?).
+   * - Space: (?).
+   * @returns {boolean} Whether the list has a loop or not.
+   */
+  hasLoop() {}
+
+  /**
+   * Removes all the nodes that have a negative integer as their data.
+   * - Time: (?).
+   * - Space: (?).
+   * @returns {SinglyLinkedList} This list after the negatives are removed.
+   */
+  removeNegatives() {}
+
+  /**
    * Finds the node with the smallest number as data and moves it to the front
    * of this list.
    * - Time: O(2n) n = list length -> O(n) linear,
