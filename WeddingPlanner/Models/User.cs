d07df0fdd04002b7f843321b08f18a1c44a756f2
Neil M <neilm813@gmail.com>
2022-05-18 12:03:35 -0700

    rm comment

--- WeddingPlanner/Models/User.cs
+++ WeddingPlanner/Models/User.cs
@@ -1,8 +1,3 @@
-/* How to disable warning just in specific files.
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-*/
-
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
